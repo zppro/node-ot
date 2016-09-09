8fad8f2da2144ecff8bff107d4534bb2f0ab557b
zppro <zpprozppro@gmail.com>
2016-09-09 16:24:51 +0800

    0.0.3c

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ import path from 'path';
 import qr from 'qr-image';
 import sharp from 'sharp';
 import co from 'co';
+import streamBuffers from 'stream-buffers';
+import streamToArray from 'stream-to-array';
+
 
 var server = http.createServer(function (req, res) {
     co(function*() {
@@ -18,13 +21,24 @@ var server = http.createServer(function (req, res) {
 
                 var img = qr.image(text, {size: 10});
 
-                let buffer = yield sharp(path.join(__dirname,'img/05.jpg')).toBuffer()
+                let arrQR = yield streamToArray(img);
+                let buffers = arrQR.map(function(o) {
+                    return o instanceof Buffer ? o : new Buffer(o);
+                });
+                let bufferQR = Buffer.concat(buffers);
 
-                
+                let buffer = yield sharp(bufferQR).overlayWith(path.join(__dirname,'img/05.jpg')).toBuffer();
 
+                let myReadableStreamBuffer = new streamBuffers.ReadableStreamBuffer({
+                    frequency: 10,       // in milliseconds.
+                    chunkSize: 2048     // in bytes.
+                });
+
+                myReadableStreamBuffer.put(buffer);
                 res.writeHead(200, {'Content-Type': 'image/png'});
-                img.pipe(res);
+                myReadableStreamBuffer.pipe(res);
             } catch (e) {
+                console.log(e);
                 res.writeHead(414, {'Content-Type': 'text/html'});
                 res.end('<h1>414 Request-URI Too Large' + text + '</h1>');
             }
