984a1c137007b0fb5a9ec80ef12e7a66e969ba90
zppro <zpprozppro@gmail.com>
2016-09-09 16:43:44 +0800

    0.0.4

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import path from 'path';
 import qr from 'qr-image';
 import sharp from 'sharp';
 import co from 'co';
-import streamBuffers from 'stream-buffers';
 import streamToArray from 'stream-to-array';
 
 
@@ -19,24 +18,17 @@ var server = http.createServer(function (req, res) {
             let text = url.parse(req.url, true).query.text;
             try {
 
-                var img = qr.image(text, {size: 10});
-
+                let img = qr.image(text, {size: 10,ec_level:'H'});
                 let arrQR = yield streamToArray(img);
                 let buffers = arrQR.map(function(o) {
                     return o instanceof Buffer ? o : new Buffer(o);
                 });
                 let bufferQR = Buffer.concat(buffers);
+                let iconBuffer = yield sharp(path.join(__dirname,'img/05.jpg')).resize(57,57).toBuffer();
+                let compositStream = sharp(bufferQR).overlayWith(iconBuffer);
 
-                let buffer = yield sharp(bufferQR).overlayWith(path.join(__dirname,'img/05.jpg')).toBuffer();
-
-                let myReadableStreamBuffer = new streamBuffers.ReadableStreamBuffer({
-                    frequency: 10,       // in milliseconds.
-                    chunkSize: 2048     // in bytes.
-                });
-
-                myReadableStreamBuffer.put(buffer);
                 res.writeHead(200, {'Content-Type': 'image/png'});
-                myReadableStreamBuffer.pipe(res);
+                compositStream.pipe(res);
             } catch (e) {
                 console.log(e);
                 res.writeHead(414, {'Content-Type': 'text/html'});
