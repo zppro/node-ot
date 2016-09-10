80833f7f67edc802c6f834d7df1edda427cede5a
zppro <zpprozppro@gmail.com>
2016-09-10 13:37:32 +0800

    0.0.6

--- src/index.js
+++ src/index.js
@@ -11,15 +11,44 @@ import co from 'co';
 import streamToArray from 'stream-to-array';
 import 'babel-polyfill';//执行模块，特别是需要用到定义的全局变量给es5使用
 
+const getEI = (eiUrl) => {
+
+    return new Promise(function(reslove,reject){
+        let urlObj = url.parse(eiUrl, true);
+        var req = http.request({
+            hostname: urlObj.host,
+            port :urlObj.port || 80,
+            path:urlObj.pathname
+        },(res)=>{
+            let body = '';
+            res.setEncoding('binary');
+            res.on('end',()=>{
+                reslove(new Buffer(body,'binary'));
+            });
+            res.on('data',(chunk)=>{
+                if(res.statusCode == 200){
+                    body+= chunk;
+                }
+            });
+
+        });
+        req.on('error',(e)=>{
+           reject(e.message);
+        });
+        req.end();
+
+    });
+};
+
 
 var server = http.createServer(function (req, res) {
     co(function*() {
 
-        if (req.url.startsWith('/?text')) {
-            let text = url.parse(req.url, true).query.text;
+        let query = url.parse(req.url, true).query;
+        if (query.text) {
             try {
-
-                let img = qr.image(text, {size: 10,ec_level:'H'});
+                res.writeHead(200, {'Content-Type': 'image/png'});
+                let img = qr.image(query.text, {size: 10,ec_level:'H'});
                 let arrQR = yield streamToArray(img);
                 let buffers = arrQR.map(function(o) {
                     return o instanceof Buffer ? o : new Buffer(o);
@@ -28,15 +57,27 @@ var server = http.createServer(function (req, res) {
                 let qrStream = sharp(bufferQR);
                 let qrInfo = yield qrStream.metadata();
 
-                let iconBuffer = yield sharp(path.join(__dirname,'img/05.jpg')).resize(Math.round(qrInfo.width/5),Math.round(qrInfo.height/5)).toBuffer();
+                let iconStream,iconBuffer;
+                if(query.ei){
+                    let eiBuffer = yield getEI(query.ei);
+                    iconStream = sharp(eiBuffer);
+                }
+                else{
+                    iconStream = sharp(path.join(__dirname,'img/05.jpg'));
+                }
+
+                iconBuffer = yield iconStream
+                    .resize(Math.round(qrInfo.width/5),Math.round(qrInfo.height/5))
+                    .background({r:255,g:255,b:255,a:1})
+                    .extend(2)
+                    .toBuffer();
                 let compositStream = qrStream.overlayWith(iconBuffer);
 
-                res.writeHead(200, {'Content-Type': 'image/png'});
                 compositStream.pipe(res);
             } catch (e) {
                 console.log(e);
                 res.writeHead(414, {'Content-Type': 'text/html'});
-                res.end('<h1>414 Request-URI Too Large' + text + '</h1>');
+                res.end('<h1>414 Request-URI Too Large' + e.message + '</h1>');
             }
         }
     });
